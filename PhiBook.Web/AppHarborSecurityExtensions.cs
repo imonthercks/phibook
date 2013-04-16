1bb0cb843b40f28e715e86e3e059eddc4c343aa4
C. J. Russell <imonthercks@gmail.com>
2013-04-15 21:44:30 -0500

    Added new Security Extensions for AppHarbor
    Created new Extension Method off of NancyModule to check for
    X-Forward-Proto header with value of "https" to determine if request was
    made via SSL

---
+++ PhiBook.Web/AppHarborSecurityExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+using Nancy.Responses;
+
+namespace PhiBook.Web
+{
+    public static class AppHarborSecurityExtensions
+    {
+        /// <summary>
+        /// This module requires https.
+        /// </summary>
+        /// <param name="module">The <see cref="NancyModule"/> that requires HTTPS.</param>
+        public static void RequiresXForwardProtoHeader(this NancyModule module)
+        {
+            module.RequiresXForwardProtoHeader(true);
+        }
+
+        /// <summary>
+        /// This module requires https.
+        /// </summary>
+        /// <param name="module">The <see cref="NancyModule"/> that requires HTTPS.</param>
+        /// <param name="redirect"><see langword="true"/> if the user should be redirected to HTTPS if the incoming request was made using HTTP, otherwise <see langword="false"/> if <see cref="HttpStatusCode.Forbidden"/> should be returned.</param>
+        public static void RequiresXForwardProtoHeader(this NancyModule module, bool redirect)
+        {
+            module.Before.AddItemToEndOfPipeline(RequiresHttps(redirect));
+        }
+
+        private static Func<NancyContext, Response> RequiresHttps(bool redirect)
+        {
+            return (ctx) =>
+            {
+                Response response = null;
+                var request = ctx.Request;
+                if (!request.Url.IsSecure)
+                {
+                    if (redirect && ctx.Request.Headers["X-Forwarded-Proto"].FirstOrDefault(x => x == "https") != null)
+                    {
+                        var redirectUrl = request.Url.Clone();
+                        redirectUrl.Scheme = "https";
+                        response = new RedirectResponse(redirectUrl.ToString());
+                    }
+                    else
+                    {
+                        response = new Response { StatusCode = HttpStatusCode.Forbidden };
+                    }
+                }
+
+                return response;
+            };
+        }
+    }
+}
